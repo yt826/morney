2791f92740dbc0025730529aac33a8d42f3d262b
yangtao <1772492146@qq.com>
2020-11-10 21:01:47 +0800

    添加404“

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <router-link to="/money">money</router-link>
+    <router-link to="/money">记账</router-link>
      |
-    <router-link to="/labels">labels</router-link>
+    <router-link to="/labels">标签</router-link>
      |
-    <router-link to="/statistics">statistics</router-link>
+    <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
+
 export default {
   name: 'Nav'
 };
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>没有找到这个页面，请检查你的网址是否正确！</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
