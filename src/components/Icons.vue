973734a8d1cc7e0b53c393dab439d39bf877399e
yangtao <1772492146@qq.com>
2020-11-12 00:14:51 +0800

    添加nav样式

---
+++ src/components/Icons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <svg class="icon">
+      <use :xlink:href="'#'+name" />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+let importAll = (requireContext:__WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try{importAll(require.context('../assets/icons',true,/\.svg$/))
+}catch{console.log(Error);}
+export default {
+  props:['name'],
+  name: 'Icons'
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 32px;
+  height: 32px;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,40 @@
 <template>
-  <div class="nav">
-
-    <router-link to="/money">记账</router-link>
-     |
-    <router-link to="/labels">标签</router-link>
-     |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item">
+      <Icons name='money'/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item">
+      <Icons name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icons name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-
 export default {
-  name: 'Nav'
+  name: 'Nav',
 };
 
 </script>
 
 <style lang="scss" scoped>
-.nav{
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+
+  > .item {
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 3px 0;
+  }
 }
 </style>
