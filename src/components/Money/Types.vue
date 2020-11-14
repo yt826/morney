34ca1d77e43987b1038adb5f642b1bbd86fc9b28
yangtao <1772492146@qq.com>
2020-11-14 13:03:20 +0800

    完成types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class= "type === '-' && 'selected' " @click="selectType('-')">支出</li>
+      <li :class= "type === '+' && 'selected' " @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
-  name: 'Types'
-};
+  name: 'Types',
+  props:['xxx'],
+  data() {
+    return {
+      type: '-'//
+    }
+  },
+  mounted(){
+    console.log(this.xxx)
+  },
+  methods:{
+    selectType(type){
+      if(type !== '-' && type!=="+")
+      {
+        throw new Error('type is unknown')
+      }
+      this.type = type
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
