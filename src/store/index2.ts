a567a2029eb5b15d0a68c023bf88bd85093d12a4
yangtao <1772492146@qq.com>
2020-11-24 20:16:24 +0800

    将store拆分为recordStore和tagStore

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,30 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  recordList: recordListModel.fetch(),
-  createRecordList: (record: RecordItem) => {
-    recordListModel.create(record);
-  },
-  tagList: tagListModel.fetch(),
-  findTag: function (id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  },
-  createTag: (name: string) => {
-    const massage = tagListModel.create(name);
-    if (massage === 'fail') {
-      window.alert('你已经有一个标签了！');
-    } else if (massage === 'success') {
-      window.alert('创建成功！');
-    }
-    return;
-  },
-  removeTag: (id: string) => {
-    const massage = tagListModel.remove(id);
-    return massage === 'success';
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
+ ...recordStore,
+  ...tagStore,
 };
 export default store;
